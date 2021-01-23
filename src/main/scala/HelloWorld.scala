042f5cba20d2ea7b3c56bdf041a1c8ce5317fe70
azu0nyan <nikgavgav@gmail.com>
2021-01-23 10:33:34 +0300

    refactoring

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -17,6 +17,7 @@ object HelloWorld {
 }
 class HelloWorld { // The window handle
   private var window = 0L
+
   def run(): Unit = {
     System.out.println("Hello LWJGL " + Version.getVersion + "!")
     init()
@@ -28,6 +29,16 @@ class HelloWorld { // The window handle
     glfwTerminate()
     glfwSetErrorCallback(null).free()
   }
+
+  def keyPressed(window: Long, key: Int, scancode: Int, action: Int, mods: Int): Unit = {
+    (key, action, mods) match {
+      case (GLFW_KEY_ESCAPE, GLFW_RELEASE, _) => glfwSetWindowShouldClose(window, true)
+      case (GLFW_KEY_A, GLFW_RELEASE, GLFW_MOD_SHIFT) => println("A pressed with shift")
+      case (GLFW_KEY_A, GLFW_RELEASE, _) => println("A pressed")
+      case _ =>
+    }
+  }
+
   private def init(): Unit = { // Setup an error callback. The default implementation
     // will print the error message in System.err.
     GLFWErrorCallback.createPrint(System.err).set
@@ -44,26 +55,21 @@ class HelloWorld { // The window handle
     window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
     if (window == NULL) throw new RuntimeException("Failed to create the GLFW window")
     // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-    glfwSetKeyCallback(window, (window: Long, key: Int, scancode: Int, action: Int, mods: Int) => {
-      def foo(window: Long, key: Int, scancode: Int, action: Int, mods: Int) = {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) glfwSetWindowShouldClose(window, true) // We will detect this in the rendering loop
-      }
-      foo(window, key, scancode, action, mods)
-    })
+    glfwSetKeyCallback(window, keyPressed)
+
     // Get the thread stack and push a new frame
+    val stack = stackPush
     try {
-      val stack = stackPush
-      try {
-        val pWidth = stack.mallocInt(1) // int*
-        val pHeight = stack.mallocInt(1)
-        // Get the window size passed to glfwCreateWindow
-        glfwGetWindowSize(window, pWidth, pHeight)
-        // Get the resolution of the primary monitor
-        val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor)
-        // Center the window
-        glfwSetWindowPos(window, (vidmode.width - pWidth.get(0)) / 2, (vidmode.height - pHeight.get(0)) / 2)
-      } finally if (stack != null) stack.close()
-    } // the stack frame is popped automatically
+      val pWidth = stack.mallocInt(1) // int*
+      val pHeight = stack.mallocInt(1)
+      // Get the window size passed to glfwCreateWindow
+      glfwGetWindowSize(window, pWidth, pHeight)
+      // Get the resolution of the primary monitor
+      val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor)
+      // Center the window
+      glfwSetWindowPos(window, (vidmode.width - pWidth.get(0)) / 2, (vidmode.height - pHeight.get(0)) / 2)
+    } finally if (stack != null) stack.close()
+    // the stack frame is popped automatically
 
     // Make the OpenGL context current
     glfwMakeContextCurrent(window)
@@ -79,12 +85,10 @@ class HelloWorld { // The window handle
     // bindings available for use.
     GL.createCapabilities
     // Set the clear color
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+    glClearColor(0.2f, 0.7f, 0.7f, 0.0f)
     // Run the rendering loop until the user has attempted to close
     // the window or has pressed the ESCAPE key.
-    while ( {
-      !glfwWindowShouldClose(window)
-    }) {
+    while (!glfwWindowShouldClose(window)) {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) // clear the framebuffer
 
       glfwSwapBuffers(window) // swap the color buffers
